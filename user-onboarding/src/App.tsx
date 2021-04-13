7a6a690f943f97b72a34770fdc779e9b8134c234
MatthiasBakken <matthias.bakken@gmail.com>
2021-04-13 08:21:08 -0700

    finished project using formik, yup, axios, uuid and typescript

---
+++ user-onboarding/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignupForm from "./SignupForm";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default App;
